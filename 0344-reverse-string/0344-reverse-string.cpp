7905dd06c9e475b672d1473529cf635620ff14d2
Rupam Maiti <maitirupam63@gmail.com>
2026-08-07 15:54:35 +0530

    Time: 0 ms (100%), Space: 27.2 MB (53.59%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        // int n=s.size();
+        // int index=0;
+        // for(int i=n-1;i>0;i--){
+        //     s[index++]=s[i];
+        //   }
+        reverse(s.begin(),s.end());
+        
+      
+    }
+};
