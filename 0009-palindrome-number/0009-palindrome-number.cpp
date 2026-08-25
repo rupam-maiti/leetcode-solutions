1e60787f0da05b11f9995197f022da2ad873a70e
Rupam Maiti <maitirupam63@gmail.com>
2026-08-25 16:34:04 +0530

    Time: 4 ms (30.43%), Space: 10.9 MB (11.79%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s=to_string(x);
+        int n=s.length();
+        int st = 0;
+        int end = n - 1;
+        while (st <end) {
+            if (s[st] != s[end]) {
+                return false;
+            }
+                st++;
+                end--;
+            }
+            
+        
+       return true;
+    }
+};
