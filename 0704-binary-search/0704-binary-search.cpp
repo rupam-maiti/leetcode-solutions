4f6d77268daec9c26d2087e17489b4e75e3ad8d7
Rupam Maiti <maitirupam63@gmail.com>
2026-09-09 23:31:07 +0530

    Time: 0 ms (100%), Space: 31.3 MB (46.05%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+        int binarySearch(vector<int>&arr,int tar,int st,int end){
+            if(st<=end){
+                int mid=st+(end-st)/2;
+                if(arr[mid]==tar) return mid;
+                else if(arr[mid]<=tar) 
+                return binarySearch(arr,tar,mid+1,end);
+                else {
+                return binarySearch(arr,tar,st,mid-1);
+                } 
+            }
+            return -1;
+        }
+
+
+    int search(vector<int>& arr, int tar) {
+        return binarySearch(arr,tar,0,arr.size()-1);
+    }
+};
